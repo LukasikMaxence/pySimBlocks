5d7d6573b1ae6b9e58ec1968659de755015d7561
Lukasik <maxence.lukasik.pro@gmail.com>
2026-06-08 10:59:13 +0200

    feat(gui): add GroupIn/GroupOut proxy graphics for internal group view

---
+++ pySimBlocks/gui/group_ports.py
@@ -0,0 +1,10 @@
+# ******************************************************************************
+#                                  pySimBlocks
+#                     Copyright (c) 2026 Université de Lille & INRIA
+# ******************************************************************************
+
+"""Constants for visual-only group port palette entries."""
+
+GROUP_PORTS_CATEGORY = "group_ports"
+GROUP_IN_TYPE = "In"
+GROUP_OUT_TYPE = "Out"
